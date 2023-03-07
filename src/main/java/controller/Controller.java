87daac20c43485aa81b2590d65805c8174c572e1
Emanuele Dajko <ema.unibo@gmail.com>
2023-03-07 19:53:43 +0100

    add Controller Interface

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,27 @@
+package controller;
+
+public interface Controller {
+    
+    /**
+     *  
+     */
+    // void registerAsObserver();
+    // void notifyInput(...);
+
+    /**
+     * this method run the Main Loop: 
+     */
+    void startMainLoop();
+
+
+
+
+
+
+    
+
+
+
+
+
+}
