20213601c22bb02e2e95a0e82d1bce85910e17e2
Lorenzo Prati <lorenzo.prati3@studio.unibo.it>
2023-03-13 21:55:01 +0100

    update Player classes

---
+++ src/main/java/model/dynamic/player/Player.java
@@ -0,0 +1,14 @@
+package model.dynamic.player;
+
+/**
+ * An interface to model the Player
+ */
+public interface Player { 
+
+    int getCoins();
+
+    Health getHealth();
+
+    void takeDamage(int amount);
+    
+}
---
+++ src/main/java/model/dynamic/player/PlayerImpl.java
@@ -0,0 +1,122 @@
+package model.dynamic.player;
+
+import java.util.LinkedList;
+
+import org.locationtech.jts.geom.Coordinate;
+
+import model.common.ObjectType;
+import model.common.State;
+import model.dynamic.AbstractDynamicObject;
+import model.dynamic.LivingCharacter;
+import model.dynamic.enemies.Enemy;
+import model.items.Item;
+import model.physics.CollisionBox;
+import model.rooms.Room;
+
+public class PlayerImpl extends AbstractDynamicObject implements Player, LivingCharacter {
+
+    private Health healthComponent;
+    private int coins;
+    private boolean attacking;
+    private boolean shooting;
+    private int meeleDamage;
+    private int ammunition;
+
+    public PlayerImpl(Coordinate position, Integer id, ObjectType type, State state, CollisionBox collisionBox,
+            Room room,
+            int height, int width) {
+        super(position, id, type, state, collisionBox, room, height, width);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void update(long dt) {
+        this.updateState();
+        if (this.isMoving()) {
+            this.move(dt);
+            if (this.isCollidingWithWalls()) {
+                this.bounceBack();
+            }
+        }
+        for (var o : new LinkedList<>(this.getRoom().getObjects())) {
+            if (this.getCollisionBox().intersects(o.getCollisionBox())) {
+                if (o instanceof Item) {
+                    Item i = (Item) o;
+                    if (true /* i.canBeUsedOn(this) */) {
+                        i.applyEffect(this);
+                    }
+                } else if (o instanceof Enemy && this.attacking) {
+                    Enemy e = (Enemy) o;
+                    e.takeDamage(this.meeleDamage);
+                }
+            }
+        }
+        if (this.isShooting()) {
+            // check delay
+            // this.getRoom().addObjects(/*new Bullet(this) */);
+            // ammunition--
+        }
+    }
+
+    @Override
+    public void updateState() {
+        if (this.isMoving()) {
+            switch (this.getDirection()) {
+                case UP:
+                    this.setState(State.WALK_UP);
+                case DOWN:
+                    this.setState(State.WALK_DOWN);
+                case LEFT:
+                    this.setState(State.WALK_LEFT);
+                case RIGHT:
+                    this.setState(State.WALK_RIGHT);
+            }
+        }
+        if (this.isAttacking()) {
+            switch (this.getDirection()) {
+                case UP:
+                    this.setState(State.ATTACK_UP);
+                case DOWN:
+                    this.setState(State.ATTACK_DOWN);
+                case LEFT:
+                    this.setState(State.ATTACK_LEFT);
+                case RIGHT:
+                    this.setState(State.ATTACK_RIGHT);
+            }
+        }
+        if (!this.isMoving() || !this.isAttacking()) {
+            this.setState(State.IDLE);
+        }
+    }
+
+    public Health getHealth() {
+        return healthComponent;
+    }
+
+    public int getCoins() {
+        return coins;
+    }
+
+    public void setCoins(int amount) {
+        this.coins += amount;
+    }
+
+    public boolean isAttacking() {
+        return this.attacking;
+    }
+
+    @Override
+    public boolean isAlive() {
+        return this.getHealth().getValue() > 0;
+    }
+
+    private boolean isShooting() {
+        return this.shooting;
+    }
+
+    @Override
+    public void takeDamage(int amount) {
+        this.getHealth().decrease(amount);
+    }
+
+}
