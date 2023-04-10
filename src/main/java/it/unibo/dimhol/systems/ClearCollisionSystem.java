8d30da337a0914a4b9b9cdd7de60ff77e93cd375
Lorenzo Prati <lorenzo.prati3@studio.unibo.it>
2023-04-10 13:20:52 +0200

    Add basic collision handling in CollisionSystem, add ClearCollisionSystem

---
+++ src/main/java/it/unibo/dimhol/systems/ClearCollisionSystem.java
@@ -0,0 +1,22 @@
+package it.unibo.dimhol.systems;
+
+import it.unibo.dimhol.World;
+import it.unibo.dimhol.components.CollisionComponent;
+import it.unibo.dimhol.components.Component;
+import it.unibo.dimhol.entity.Entity;
+
+/**
+ * A system to remove all collision components before next update cycle.
+ */
+public class ClearCollisionSystem extends AbstractSystem {
+
+    public ClearCollisionSystem(World w, Class<? extends Component>... comps) {
+        super(w, comps);
+    }
+
+    @Override
+    public void process(Entity e, long dt) {
+        var cc = (CollisionComponent) e.getComponent(CollisionComponent.class);
+        e.removeComponent(cc);
+    }
+}
--- src/main/java/it/unibo/dimhol/systems/CollisionSystem.java
+++ src/main/java/it/unibo/dimhol/systems/CollisionSystem.java
@@ -2,8 +2,12 @@ package it.unibo.dimhol.systems;
 
 import it.unibo.dimhol.World;
 import it.unibo.dimhol.components.BodyComponent;
+import it.unibo.dimhol.components.CollisionComponent;
 import it.unibo.dimhol.components.Component;
+import it.unibo.dimhol.components.PositionComponent;
 import it.unibo.dimhol.entity.Entity;
+
+import java.lang.reflect.GenericArrayType;
 
 /**
  * A system to register all collisions between entities.
@@ -16,6 +20,26 @@ public class CollisionSystem extends AbstractSystem {
 
     @Override
     public void process(Entity e, long dt) {
+        var p1 = (PositionComponent) e.getComponent(BodyComponent.class);
         var b1 = (BodyComponent) e.getComponent(BodyComponent.class);
+        for (var other : this.world.getEntities()) {
+            if (!other.equals(e) && other.hasComponent(BodyComponent.class)) {
+                var p2 = (PositionComponent) other.getComponent(PositionComponent.class);
+                var b2 = (BodyComponent) other.getComponent(BodyComponent.class);
+                if (this.collisionHappens(p1,p2,b1,b2)) {
+                    /**
+                     * to do: handle physical collision
+                     */
+                    if (!e.hasComponent(CollisionComponent.class)) {
+                        e.addComponent(new CollisionComponent(other));
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean collisionHappens(PositionComponent p1, PositionComponent p2, BodyComponent b1, BodyComponent b2) {
+        return b1.getBs().getShape(p1.getX(),p1.getY())
+                .intersects(b2.getBs().getShape(p2.getX(),p2.getY()));
     }
 }
