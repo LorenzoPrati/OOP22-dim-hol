ef3fb8b3fadc2f351257b7763da6920001c8bf00
Emanuele Dajko <ema.unibo@gmail.com>
2023-04-15 20:03:02 +0200

    update Tile

--- src/main/java/it/unibo/dimhol/Map/Tile.java
+++ src/main/java/it/unibo/dimhol/Map/Tile.java
@@ -1,2 +1,15 @@
-package it.unibo.dimhol.Map;public class Tile {
+package it.unibo.dimhol.Map;
+public class Tile {
+    //TODO: private List<Effect> effects;
+
+    private boolean collidable;
+
+    public Tile(boolean collidable) {
+        this.collidable = collidable;
+    }
+
+    public boolean isCollidable() {
+        return collidable;
+    }
+
 }
