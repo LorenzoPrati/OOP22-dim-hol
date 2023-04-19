870a936479b11c75632a44a402cac6f9e73b1059
Emanuele Dajko <ema.unibo@gmail.com>
2023-04-19 23:20:15 +0200

    Add Tile

---
+++ src/main/java/it/unibo/dimhol/map/Tile.java
@@ -0,0 +1,33 @@
+package it.unibo.dimhol.map;
+
+/**
+ * A Tile represents a single tile in a game map, with information about whether it is collidable.
+ */
+public class Tile {
+    /**
+     * Whether this Tile is collidable.
+     */
+    private final boolean collidable;
+
+    /**
+     * Constructs a new Tile with the specified collidability.
+     *
+     * @param collidable whether this Tile should be collidable.
+     */
+    public Tile(final boolean collidable) {
+        this.collidable = collidable;
+    }
+
+    /**
+     * Returns whether this Tile is collidable.
+     *
+     * @return true if this Tile si collidable, false otherwise.
+     */
+    public boolean isCollidable() {
+        return collidable;
+    }
+}
+
+
+
+
