f57e3f9bc44d4878dd411fa584e77bf3b332e5c5
EmanueleDajko <emanuele.dajko@studio.unibo.it>
2023-04-20 18:51:04 +0200

    add Room interface

---
+++ src/main/java/it/unibo/dimhol/map/Room.java
@@ -0,0 +1,22 @@
+package it.unibo.dimhol.map;
+
+/**
+ * An interface for different game rooms    .
+ */
+public interface Room {
+
+    /**
+     * Type one: Normal Room
+     */
+    void normalRoom();
+
+    /**
+     * Type two: Shop Room
+     */
+     void ShopRoom();
+
+    /**
+     * Type three: Boss Room
+     */
+    void BossRoom();
+}
