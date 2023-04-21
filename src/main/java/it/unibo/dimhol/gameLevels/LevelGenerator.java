ea67349d0ea892431b15023b321a8f34705b7595
Emanuele Dajko <ema.unibo@gmail.com>
2023-04-22 01:32:54 +0200

    update the initialization of LevelGenerator but doesn not complete it

--- src/main/java/it/unibo/dimhol/gameLevels/LevelGenerator.java
+++ src/main/java/it/unibo/dimhol/gameLevels/LevelGenerator.java
@@ -1,47 +1,124 @@
 package it.unibo.dimhol.gameLevels;
 
 import it.unibo.dimhol.World;
+import it.unibo.dimhol.components.PlayerComponent;
 import it.unibo.dimhol.entity.Entity;
 import it.unibo.dimhol.entity.GenericFactory;
 import it.unibo.dimhol.events.AddEntityEvent;
 import it.unibo.dimhol.map.LoadMap;
 import it.unibo.dimhol.map.LoadMapImpl;
+import it.unibo.dimhol.map.Tile;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class LevelGenerator {
     private World world;
-    private LoadMap iter;
-    private GenericFactory genericFactory
-    private LoadMapImpl map;
+    private LoadMap loadMap = new LoadMapImpl("src/main/java/it/unibo/dimhol/map/mapResources/FirstTryTiled.xml");
+    private GenericFactory genericFactory = new GenericFactory();
+    private Tile[][] map;
     private int currentLevel;
+    private Entity player;
 
-    public LevelGenerator(World world, GenericFactory genericFactory) {
+    private LevelGenerator(World world) {
         this.world = world;
-        this.genericFactory = genericFactory;
         currentLevel = 0;
     }
 
-    //Load the map:
-    public void loadMap(String fileName) {
-        LoadMapImpl loadMap = new LoadMapImpl(fileName);
+    //Change the level:
+//    public void changeLevel() {
+        //Saving player
+        //Clear the room
+        //Load the map
+        //Finds walkable tiles positions -> empty or not
+        //Puts player
+        //Puts enemies
+
+        /**
+         * Changes the current level.
+         */
+        public void changeLevel() {
+            savePlayer();
+            clearEntities();
+            loadMap();
+            List<Tile> walkableTiles = findWalkableTile();
+            int playerIndex = new Random().nextInt(walkableTiles.size());
+            Tile playerTile = walkableTiles.get(playerIndex);
+            putPlayer();
+            putEnemies();
+        }
+
+    /**
+     * Saves the player's entity.
+     */
+    private void savePlayer() {
+        for (Entity entity : world.getEntities()) {
+            if(entity.hasComponent(PlayerComponent.class)) {
+                player = entity;
+            }
+        }
+    }
+
+    /**
+     * Clears all entities except for the player.
+     */
+    private void clearEntities() {
+        world.getEntities().clear();
+    }
+
+    /**
+     * Loads the map into the level generator.
+     */
+    private void loadMap() {
         map = loadMap.getMap();
     }
-    //Add entity:
-    public void addEntity(Entity entity, int x, int y) {
-        world.notifyEvent(new AddEntityEvent(entity)); //x and y are already specified.
-    }
-    //Generate Level:
-    public void generateLevel() {
-        //Load map:
-        loadMap("src/main/java/it/unibo/dimhol/map/mapResources/FirstTryTiled.xml");
-        //Set the map:
-        world.setMap(map); //to be added to the world.
-//        world.setGraphicalMap();
-        //Add player and enemies:
-        addEntity(genericFactory.createPlayer(5, 5));
-        addEntity(genericFactory.createShooterEnemy(10,10));
 
-        //Change level:
-        world.changeLevel(); //to be added to the world.
+    /**
+     * Finds all the walkable tiles in the current map.
+     *
+     * @return A list of all the walkable tiles.
+     */
+    private List<Tile> getWalkableTiles() {
+        List<Tile> walkableTiles = new ArrayList<>();
+        for (int i = 0;)
+
+        return null;
+    }
+
+    /**
+     * Puts the player entity at the specified position.
+     *
+     * @param x the x-coordinate of the player entity.
+     * @param y the y-coordinate of the player entity.
+     */
+    private void putPlayer(double x, double y) {
+    }
+
+    /**
+     * Puts the enemies entities randomly around the map.
+     *
+     * @param walkableTiles The list of walkable tiles in the map.
+     */
+    private void putEnemies(List<Tile> walkableTiles) {
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
