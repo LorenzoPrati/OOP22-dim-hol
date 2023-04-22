f91538e05d31372d0808c9733c5af330044bcf4b
Emanuele Dajko <ema.unibo@gmail.com>
2023-04-22 12:04:25 +0200

    add the changeLevel() implementation and others

--- src/main/java/it/unibo/dimhol/gameLevels/LevelGenerator.java
+++ src/main/java/it/unibo/dimhol/gameLevels/LevelGenerator.java
@@ -9,17 +9,21 @@ import it.unibo.dimhol.map.LoadMap;
 import it.unibo.dimhol.map.LoadMapImpl;
 import it.unibo.dimhol.map.Tile;
 
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.awt.Point;
 
 public class LevelGenerator {
+    private static final int ENEMY_DENSITY = 50;
     private World world;
     private LoadMap loadMap = new LoadMapImpl("src/main/java/it/unibo/dimhol/map/mapResources/FirstTryTiled.xml");
     private GenericFactory genericFactory = new GenericFactory();
     private Tile[][] map;
     private int currentLevel;
     private Entity player;
+    private Entity enemy;
 
     private LevelGenerator(World world) {
         this.world = world;
@@ -45,8 +49,8 @@ public class LevelGenerator {
             List<Tile> walkableTiles = getWalkableTiles();
             int playerIndex = new Random().nextInt(walkableTiles.size());
             Tile playerTile = walkableTiles.get(playerIndex);
-//            putPlayer(); //TODO: continue
-//            putEnemies();//TODO: continue
+            putPlayer(playerTile.getX(5.0), playerTile.getY(5.0)); //TODO: to be solved
+            putEnemies(walkableTiles, playerIndex);
         }
 
     /**
@@ -81,8 +85,13 @@ public class LevelGenerator {
      */
     private List<Tile> getWalkableTiles() {
         List<Tile> walkableTiles = new ArrayList<>();
-//        for (int i = 0;)
-//TODO: continue
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                if (map[i][j].isCollidable()) {
+                    walkableTiles.add(map[i][j]);
+                }
+            }
+        }
         return walkableTiles;
     }
 
@@ -93,15 +102,28 @@ public class LevelGenerator {
      * @param y the y-coordinate of the player entity.
      */
     private void putPlayer(double x, double y) {
-        //TODO: continue
+        Entity playerEntity = genericFactory.createPlayer(x, y);
+        world.addEntity(playerEntity);
+//        world.setPlayer(playerEntity);
     }
 
     /**
      * Puts the enemies entities randomly around the map.
      *
      * @param walkableTiles The list of walkable tiles in the map.
+     * @param playerIndex The index of the tile where the player will be placed.
      */
-    private void putEnemies(List<Tile> walkableTiles) {
-        //TODO: continue
+    private void putEnemies(List<Tile> walkableTiles, int playerIndex) {
+        //Get the number of enemies to place:
+        int numEnemies = (int) Math.ceil((walkableTiles.size() - 1) * ENEMY_DENSITY); // It calculates the number of enemies to place based on the density defined in ENEMY_DENSITY
+        //Remove the player tile from the walkable tiles:
+        walkableTiles.remove(playerIndex);
+        for (int i = 0; i < numEnemies; i++) {
+            //Get a random tile index from the list of walkable tiles.
+            int randomIndex = (int) (Math.random() * walkableTiles.size()); //randomly selects tiles from the list to place enemies on
+            world.addEntity(enemy);
+            //remove the tile form the list of walkable tiles so an enemy cannot be placed here.
+            walkableTiles.remove(randomIndex);
+        }
     }
 }
