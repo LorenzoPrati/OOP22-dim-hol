e278e929f3f532ef3c3b4b8754fbcd75c1069216
Lorenzo Prati <lorenzo.prati3@studio.unibo.it>
2023-04-22 09:12:44 +0200

    feat: add player states

--- src/main/java/it/unibo/dimhol/systems/PlayerInputSystem.java
+++ src/main/java/it/unibo/dimhol/systems/PlayerInputSystem.java
@@ -1,6 +1,7 @@
 package it.unibo.dimhol.systems;
 
 import it.unibo.dimhol.World;
+import it.unibo.dimhol.components.AnimationComponent;
 import it.unibo.dimhol.components.Component;
 import it.unibo.dimhol.components.MovementComponent;
 import it.unibo.dimhol.components.PlayerComponent;
@@ -19,19 +20,33 @@ public class PlayerInputSystem extends AbstractSystem {
     @Override
     public void process(Entity e, long dt) {
         var mov = (MovementComponent) e.getComponent(MovementComponent.class);
-        mov.setEnabled(false);
-        mov.setDir(new Vector2D(0,0));
+        var an = (AnimationComponent) e.getComponent(AnimationComponent.class);
         var input = this.world.getInput();
         if (input.anyMoveKeyPressed()) {
             mov.setEnabled(true);
             if (input.isUp()) {
                 mov.setDir(new Vector2D(0,-1));
+                an.setState("walking up");
             } else if (input.isDown()) {
                 mov.setDir(new Vector2D(0,1));
+                an.setState("walking down");
             } else if (input.isLeft()) {
                 mov.setDir(new Vector2D(-1,0));
+                an.setState("walking left");
             } else if (input.isRight()) {
                 mov.setDir(new Vector2D(1,0));
+                an.setState("walking right");
+            }
+        } else {
+            mov.setEnabled(false);
+            if (mov.getDir().equals(new Vector2D(0,-1))) {
+                an.setState("idle up");
+            } else if (mov.getDir().equals(new Vector2D(0,1))) {
+                an.setState("idle down");
+            } else if (mov.getDir().equals(new Vector2D(-1,0))) {
+                an.setState("idle left");
+            } else if (mov.getDir().equals(new Vector2D(1,0))) {
+                an.setState("idle right");
             }
         }
     }
