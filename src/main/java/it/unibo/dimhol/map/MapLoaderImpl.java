42760cf8db134848b78bf6787cc2a1bf162054ac
Emanuele Dajko <ema.unibo@gmail.com>
2023-04-24 14:11:56 +0200

    add attributes, now print error and update the for statement

--- src/main/java/it/unibo/dimhol/map/MapLoaderImpl.java
+++ src/main/java/it/unibo/dimhol/map/MapLoaderImpl.java
@@ -1,6 +1,7 @@
 package it.unibo.dimhol.map;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -17,93 +18,97 @@ import java.util.List;
 /**
  * The class is responsible for parsing an XML file and loading a map from it.
  */
-public class MapLoaderImpl implements LoadMap {
+public class MapLoaderImpl implements MapLoad {
     private Tile[][] map;
-    private int width;
-    private int height;
-    private int tileWidth;
-    private int tileHeight;
-    private List<Tile[][]> mapTileLayers;
+    private final int width;
+    private final int height;
+    private final int tileWidth;
+    private final int tileHeight;
+    private final List<Tile[][]> mapTileLayers;
+    NodeList layerNodeList;
+    Element layerElement;
 
     /**
-     * Constructor for LoadMapImpl that loads a map from an XML file.
-     * @param fileName The name of the XML file to load the map from.
+     * Constructor for MapLoaderImpl.
+     *
+     * @param tileWidth
+     * @param tileHeight
+     * @param width
+     * @param height
+     * @param mapTileLayers
      */
-    public MapLoaderImpl(final String fileName) {
+    public MapLoaderImpl(int tileWidth, int tileHeight, int width, int height, List<Tile[][]> mapTileLayers) {
+        this.tileWidth = tileWidth;
+        this.tileHeight = tileHeight;
+        this.width = 0;
+        this.height = 0;
+        this.mapTileLayers = new ArrayList<>();
+    }
+
+    /**
+     * Load a map from a given XML file.
+     *
+     * @param fileName The name of the XML file to load the map from.
+     * @return true if the map has been successfully loaded, false otherwise.
+     */
+    public MapLoaderImpl loadMapFromXmlFile(final String fileName) {
         try {
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(new File(fileName));
-            /**
-             * Normalize the document by removing empty spaces and combining adjacent text nodes.
-             */
+            //Normalize the document by removing empty spaces and combining adjacent text nodes.
             doc.getDocumentElement().normalize();
-            /**
-             * Get the root element of the document.
-             */
-            var rootElement = doc.getDocumentElement();
-            tileWidth = Integer.parseInt(rootElement.getAttribute("tilewidth"));
-            tileHeight = Integer.parseInt(rootElement.getAttribute("tileheight"));
-            /**
-             * Get the list of all layer.
-             */
-            NodeList dataNodeList = doc.getElementsByTagName("data");
-            /**
-             * Get the list of all the layer elements into the document.
-             */
+            Element rootElement = doc.getDocumentElement();
+            int width = Integer.parseInt(rootElement.getAttribute("width"));
+            int height = Integer.parseInt(rootElement.getAttribute("height"));
+            int tileWidth = Integer.parseInt(rootElement.getAttribute("tilewidth"));
+            int tileHeight = Integer.parseInt(rootElement.getAttribute("tileheight"));
+
             NodeList layerNodeList = doc.getElementsByTagName("layer");
-            /**
-             * Get the first layer element by using "item(0)" and store it in a "node" variable "layerNode".
-             */
-            mapTileLayers = new ArrayList<>();
+            if (layerNodeList.getLength() == 0)
+                return null;
+
+            List<Tile[][]> mapTileLayers = new ArrayList<>();
             for (int i = 0; i < layerNodeList.getLength(); i++) {
-                mapTileLayers.add(getTileMatrix(layerNodeList.item(i)));
+                mapTileLayers.add(getTileMatrix(layerNodeList.item(i), width, height));
             }
-            Node layerNode1 = layerNodeList.item(1);
-            /**
-             * Get the first child of the data element, which contains the matrix data as string.
-             */
-            Node stringMatrix = dataNodeList.item(0).getFirstChild(); //layer 0
-            /**
-             * Print the name of the root element.
-             */
-            System.out.println("Root element:" + rootElement.getNodeName());
-            System.out.println("Map parsed:");
-            System.out.println(stringMatrix.getTextContent());
-            System.out.println("Print Successful!");
-            this.map = new Tile[height][width];
+
+            return new MapLoaderImpl(width, height, tileWidth, tileHeight, mapTileLayers);
         } catch (ParserConfigurationException | SAXException | IOException e) {
             e.printStackTrace();
+            System.out.println("ERROR: The map has not been parsed!");
         }
+        return null;
     }
-    private Tile[][] getTileMatrix(Node item) {
-        String[] line = item.getTextContent().split("[\n|,]");
+
+    private Tile[][] getTileMatrix(Node item, int width, int height) {
+        Element layerElement = (Element) item;
+        NodeList propertyNodes = layerElement.getElementsByTagName("property");
+        Element dataElement = (Element) layerElement.getElementsByTagName("data").item(0);
+
+        String[] line = dataElement.getFirstChild().getTextContent().split("[\n|,]");
         List<String> nline = Arrays.stream(line).filter(e -> !e.equals("")).toList();
         Tile[][] matrix = new Tile[width][height];
 
         int k = 0;
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                if (Integer.parseInt((nline.get(k))) == 1) {
-                    matrix[i][j] = new Tile(false);
-                } else matrix[i][j] = new Tile(true);
+                for (int l = 0; l < propertyNodes.getLength(); l++) {
+                    Element property = (Element) propertyNodes.item(l);
+                    if (Integer.parseInt((nline.get(k))) == Integer.parseInt(property.getAttribute("tileMapIdInt"))) {
+
+                        if (property.hasAttribute("walkableBool") && property.hasAttribute("tileSetIdInt")) {
+                            matrix[i][j] = new Tile(Integer.parseInt(property.getAttribute("tileSetIdInt")),
+                                    Boolean.parseBoolean(property.getAttribute("walkableBool")));
+                        }
+                    }
+                }
+                k++;
             }
+
         }
         return matrix;
     }
-
-//    /**
-//     * Test it:
-//     * @param args
-//     */
-//    public static void main(final String[] args) {
-//        LoadMapImpl parser = new LoadMapImpl("src/main/java/it/unibo/dimhol/map/mapResources/FirstTryTiled.xml");
-//        Tile[][] map = parser.getMap();
-//        int width = parser.getWidth();
-//        int height = parser.getHeight();
-//        int tileWidth = parser.getTileWidth();
-//        int tileHeight = parser.getTileHeight();
-//    }
 
     @Override
     public Tile[][] getMap() {
@@ -129,7 +134,7 @@ public class MapLoaderImpl implements LoadMap {
     public int getTileHeight() {
         return tileHeight;
     }
-
+    @Override
     public List<Tile[][]> getMapTileLayers() {
         return mapTileLayers;
     }
