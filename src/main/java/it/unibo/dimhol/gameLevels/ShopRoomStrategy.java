54fc95906c30aab5cb3bc9e021783f8127baccdb
Emanuele Dajko <ema.unibo@gmail.com>
2023-04-29 19:17:16 +0200

    reverse the height with the width, now it's correct. Also add random position generati on

--- src/main/java/it/unibo/dimhol/gameLevels/ShopRoomStrategy.java
+++ src/main/java/it/unibo/dimhol/gameLevels/ShopRoomStrategy.java
@@ -8,7 +8,7 @@ import it.unibo.dimhol.map.MapLoaderImpl;
 import java.util.List;
 import java.util.Random;
 public class ShopRoomStrategy implements RoomStrategy {
-    MapLoaderImpl mapLoader;
+    MapLoaderImpl mapLoader = new MapLoaderImpl("src/main/java/it/unibo/dimhol/map/mapResources/FirstTryTiled.xml");
     World world;
     public ShopRoomStrategy(World world) {
         this.world = world;
@@ -17,14 +17,14 @@ public class ShopRoomStrategy implements RoomStrategy {
     public void generate() {
         //player and items needs to be generated in a shop room.
         GenericFactory player = new GenericFactory();
-        double playerX = new Random().nextInt(0, mapLoader.getHeight());
-        double playerY = new Random().nextInt(0, mapLoader.getWidth());
-        world.addEntity(player.createPlayer(10.0, 10.0));
+        double playerX = new Random().nextInt(0, mapLoader.getWidth());
+        double playerY = new Random().nextInt(0, mapLoader.getHeight());
+        world.addEntity(player.createPlayer(playerX, playerY));
 
         GenericFactory shopKeeper = new GenericFactory();
-        double shopKeeperX = new Random().nextInt(0, mapLoader.getHeight());
-        double shopKeeperY = new Random().nextInt(0, mapLoader.getWidth());
-        world.addEntity(player.createPlayer(30.0, 30.0));
+        double shopKeeperX = new Random().nextInt(0, mapLoader.getWidth());
+        double shopKeeperY = new Random().nextInt(0, mapLoader.getHeight());
+        world.addEntity(player.createPlayer(shopKeeperX, shopKeeperY));
     }
     @Override
     public void spawnRewards() {
