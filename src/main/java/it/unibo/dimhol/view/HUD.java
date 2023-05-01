1fd6555d26bf99fc7347f377bd77222529fff691
Elvis Perlika <elvis>
2023-05-01 12:02:13 +0200

    test: heart hud

--- src/main/java/it/unibo/dimhol/view/HUD.java
+++ src/main/java/it/unibo/dimhol/view/HUD.java
@@ -14,9 +14,9 @@ public class HUD {
     public void show(Graphics2D g2, double newTileWidth, double newTileHeight) {
         var heartImg = loader.getImage(27);
         g2.setColor(Color.RED);
-        g2.drawRect((int) newTileWidth, (int) newTileHeight, (int) (4 * newTileWidth), (int) newTileHeight / 2);
-        g2.fillRect((int) newTileWidth, (int) newTileHeight, (int) ((4 * newTileWidth) / playerMaxHealth * playerCurrentHealth), (int) newTileHeight / 2);
-        g2.drawString("Hearts: " + playerCurrentHealth + " / " + playerMaxHealth, (float) newTileWidth, (float) newTileHeight - 2);
+        for (int i = 0; i < playerCurrentHealth; i++) {
+            g2.drawImage(heartImg, (int) (i * 20 + newTileWidth), (int) newTileHeight, (int) newTileWidth * 2, (int) newTileHeight * 2, null);
+        }
 
     }
 
