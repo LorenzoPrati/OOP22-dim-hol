3cac2d0a7f62dc962d1c890f3589e2d34bd372f9
Emanuele Dajko <ema.unibo@gmail.com>
2023-05-15 02:29:52 +0200

    Changes to final the access modifier of the method.

--- src/main/java/dimhol/gamelevels/ShopRoomStrategy.java
+++ src/main/java/dimhol/gamelevels/ShopRoomStrategy.java
@@ -1,23 +1,28 @@
 package dimhol.gamelevels;
 
 import dimhol.entity.Entity;
-import dimhol.entity.factories.GenericFactory;
 import dimhol.map.MapLoaderImpl;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.List;
-import java.util.Random;
 import java.util.Set;
 
+/**
+ *
+ */
 public class ShopRoomStrategy implements RoomStrategy {
-    private MapLoaderImpl mapLoader;
+    private final MapLoaderImpl mapLoader;
 
-    public ShopRoomStrategy(MapLoaderImpl mapLoader) {
+    /**
+     *
+     * @param mapLoader
+     */
+    public ShopRoomStrategy(final MapLoaderImpl mapLoader) {
         this.mapLoader = mapLoader;
     }
 
     @Override
-    public List<Entity> generate(Entity entity, Set<Pair<Integer, Integer>> freeTiles) {
+    public final List<Entity> generate(final Entity entity, final Set<Pair<Integer, Integer>> freeTiles) {
         return null;
     }
 }
