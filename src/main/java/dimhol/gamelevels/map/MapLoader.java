72f72828327c00c8eee1072743a69c07142f66b9
Emanuele Dajko <ema.unibo@gmail.com>
2023-05-19 01:28:13 +0200

    Adds the JavaDoc.

--- src/main/java/dimhol/gamelevels/map/MapLoader.java
+++ src/main/java/dimhol/gamelevels/map/MapLoader.java
@@ -10,38 +10,44 @@ public interface MapLoader {
 
 
     /**
+     * Returns the tile layers of the map.
      *
-     * @return map tile layers.
+     * @return the map tile layers
      */
     List<Tile[][]> getMapTileLayers();
 
     /**
+     * Loads a normal room tile map.
      *
-     * @return
+     * @return the loaded normal room tile map
      */
-
     TileMap loadNormalRoom();
 
     /**
+     * Loads a normal room tile map.
      *
-     * @return
+     * @return the loaded normal room tile map
      */
     TileMap loadShopRoom();
 
     /**
+     * Loads a boss room tile map.
      *
-     * @return
+     * @return the loaded boss room tile map
      */
     TileMap loadBossRoom();
 
     /**
+     * Loads a custom map tile map.
      *
-     * @return
+     * @return the loaded custom map tile map
      */
     TileMap loadCustomMap();
 
+
     /**
      * Returns the tile map data as a {@link TileMapImpl} object.
+     *
      * @return the tile map data
      */
     TileMap getTileMap();
