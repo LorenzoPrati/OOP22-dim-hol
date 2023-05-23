088ef5ef6778d555fe696f698ec125c60fe41de3
Alessandra Versari <alessandra.versari@studio.unibo.it>
2023-05-23 19:12:19 +0200

    Add main window interface.

--- src/main/java/dimhol/view/MainWindow.java
+++ src/main/java/dimhol/view/MainWindow.java
@@ -1,42 +1,13 @@
 package dimhol.view;
 
-import dimhol.core.Engine;
+import java.awt.Dimension;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JPanel;
 
-public class MainWindow extends JFrame {
-    private final Engine engine;
-    JPanel currPanel;
-    public MainWindow(final Engine engine) {
-        this.engine = engine;
-        this.currPanel = new HomeScreen(engine);
-        this.getContentPane().add(currPanel);
-        this.setUndecorated(false);
-        this.setPreferredSize(new Dimension((int) Toolkit.getDefaultToolkit().getScreenSize().getWidth(),
-                (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight()));
-        this.pack();
-        this.setVisible(true);
-        this.setResizable(true);
-        this.setFocusable(false);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+public interface MainWindow {
 
-    public void changePanel(JPanel panel) {
-        this.remove(currPanel);
-        this.add(panel);
-        this.currPanel = panel;
-        this.repaint();
-        this.revalidate();
-    }
+    void changePanel(JPanel panel);
 
-    public void render() {
-        this.repaint();
-        Toolkit.getDefaultToolkit().sync();
-    }
-
-    public void makeFullScreen() {
-        GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().setFullScreenWindow(this);
-    }
-
+    void setDimension(Dimension dimension);
+    
 }
