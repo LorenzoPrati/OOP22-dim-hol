b1180b8dbbce9b5595d4f2ff96f0237c52722ab8
Lorenzo Prati <lorenzo.prati3@studio.unibo.it>
2023-05-24 21:47:16 +0200

    refactor: update ChangeLevelEvent

--- src/main/java/dimhol/events/ChangeLevelEvent.java
+++ src/main/java/dimhol/events/ChangeLevelEvent.java
@@ -5,16 +5,13 @@ import dimhol.core.World;
 /**
  * Gets called when a new room needs to be generated.
  */
-public class ChangeRoomEvent implements WorldEvent {
+public class ChangeLevelEvent implements WorldEvent {
 
     /**
      * {@inheritDoc}
      */
     @Override
     public void execute(final World world) {
-//       world.getLevelManager()
-//               .changeLevel(world.getEntities())
-//               .forEach(world::addEntity);
         world.changeLevel();
     }
 }
