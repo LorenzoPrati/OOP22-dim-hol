e68307c0193979f25c2b84eb515f65fc39e9a1c2
Emanuele Dajko <ema.unibo@gmail.com>
2023-05-30 00:10:44 +0200

    Creates the boss charge attack action.

---
+++ src/main/java/dimhol/logic/enemyAI/ChargeAttack.java
@@ -0,0 +1,48 @@
+/* ChargeAttack.java */
+
+package dimhol.logic.enemyAI;
+
+import dimhol.events.AddEntityEvent;
+import dimhol.events.WorldEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * It is a strategy that performs a charge attack towards the player.
+ */
+public final class ChargeAttack extends AbstractAction {
+
+    private final double chargeSpeed;
+    private final int chargeAttackDamage;
+
+    /**
+     * ChargeAttack constructor.
+     * @param chargeSpeed the speed of the charge attack
+     * @param chargeAttackDamage the damage inflicted by the charge attack
+     */
+    public ChargeAttack(final double chargeSpeed, final int chargeAttackDamage) {
+        this.chargeSpeed = chargeSpeed;
+        this.chargeAttackDamage = chargeAttackDamage;
+    }
+
+    @Override
+    public Optional<List<WorldEvent>> execute() {
+        getMovComp().setEnabled(true);
+        getMovComp().setSpeed(chargeSpeed);
+        var direction = AttackUtil.getPlayerDirection(getPlayerCentralPos(), getEnemyCentralPos());
+        getMovComp().setDir(direction);
+        if (getAi().getCurrentTime() - getAi().getPrevTime() >= getWaitingTime()) {
+            getAi().setPrevTime(getAi().getCurrentTime());
+            return chargeAttack();
+        }
+        return Optional.empty();
+    }
+
+    private Optional<List<WorldEvent>> chargeAttack() {
+        List<WorldEvent> events = new ArrayList<>();
+        events.add(new AddEntityEvent(getAttackFactory().createChargeAttack(getEnemy())));
+        return Optional.of(events);
+    }
+}
