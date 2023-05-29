8f5bf67436d3078ea1839639fade271415d42b85
Lorenzo Prati <lorenzo.prati3@studio.unibo.it>
2023-05-29 14:37:48 +0200

    feat: add clone method in Input

--- src/main/java/dimhol/view/InputListener.java
+++ src/main/java/dimhol/view/InputListener.java
@@ -2,6 +2,7 @@ package dimhol.view;
 
 import dimhol.core.Engine;
 import dimhol.core.Input;
+import dimhol.core.InputImpl;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -9,20 +10,31 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 /**
- * A class to register keyboard and mouse input.
+ * A class to listen to keyboard and mouse input.
  */
-public class InputListener implements KeyListener, MouseListener {
+public final class InputListener implements KeyListener, MouseListener {
 
     private final Engine engine;
     private final Input input;
+    private final MainWindow mainWindow;
+    private final PauseScreen pauseScreen;
 
-    public InputListener(final Engine engine, final Input input) {
+    /**
+     * Constructs an InputListener.
+     *
+     * @param engine the engine
+     * @param mainWindow the class handling the main windows of the application
+     * @param scene the class handling the game scene
+     */
+    public InputListener(final Engine engine, final MainWindow mainWindow, final Scene scene) {
         this.engine = engine;
-        this.input = input;
+        this.mainWindow = mainWindow;
+        this.input = new InputImpl();
+        this.pauseScreen = new PauseScreen(engine, scene);
     }
-    
+
     @Override
-    public void keyPressed(KeyEvent e) {
+    public void keyPressed(final KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_W -> this.input.setUp(true);
             case KeyEvent.VK_S -> this.input.setDown(true);
@@ -31,12 +43,17 @@ public class InputListener implements KeyListener, MouseListener {
             case KeyEvent.VK_Q -> this.input.setSpecialMeele(true);
             case KeyEvent.VK_Z -> this.input.setChargeFireball(true);
             case KeyEvent.VK_E -> this.input.setInteract(true);
-            case KeyEvent.VK_ESCAPE -> this.engine.stopGame();
+            case KeyEvent.VK_ESCAPE -> {
+                this.engine.stopGame();
+                this.mainWindow.changePanel(this.pauseScreen);
+            }
+            default -> {
+            }
         }
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {
+    public void keyReleased(final KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_W -> this.input.setUp(false);
             case KeyEvent.VK_S -> this.input.setDown(false);
@@ -45,42 +62,58 @@ public class InputListener implements KeyListener, MouseListener {
             case KeyEvent.VK_Q -> this.input.setSpecialMeele(false);
             case KeyEvent.VK_Z -> this.input.setChargeFireball(false);
             case KeyEvent.VK_E -> this.input.setInteract(false);
+            default -> {
+            }
         }
     }
 
     @Override
-    public void mousePressed(MouseEvent e) {
+    public void mousePressed(final MouseEvent e) {
         switch (e.getButton()) {
             case MouseEvent.BUTTON1 -> this.input.setNormalMeele(true);
             case MouseEvent.BUTTON3 -> this.input.setShoot(true);
+            default -> {
+            }
         }
     }
 
     @Override
-    public void mouseReleased(MouseEvent e) {
+    public void mouseReleased(final MouseEvent e) {
         switch (e.getButton()) {
             case MouseEvent.BUTTON1 -> this.input.setNormalMeele(false);
             case MouseEvent.BUTTON3 -> this.input.setShoot(false);
+            default -> {
+            }
         }
     }
 
+    /**
+     * Gets the Input class associated with this InputListener.
+     *
+     * @return a copy of the input
+     */
+    public Input getInput() {
+        return this.input.clone();
+    }
+
     @Override
-    public void mouseClicked(MouseEvent e) {
+    public void mouseClicked(final MouseEvent e) {
 
     }
 
     @Override
-    public void mouseEntered(MouseEvent e) {
+    public void mouseEntered(final MouseEvent e) {
 
     }
 
     @Override
-    public void mouseExited(MouseEvent e) {
+    public void mouseExited(final MouseEvent e) {
 
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {
+    public void keyTyped(final KeyEvent e) {
 
     }
+
 }
