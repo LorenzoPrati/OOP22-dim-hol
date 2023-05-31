7900f50437557e782d095df8623cf107835be6dd
Alessandra Versari <alessandra.versari@studio.unibo.it>
2023-05-31 21:29:50 +0200

    Modify abstract screen.

--- src/main/java/dimhol/view/AbstractScreen.java
+++ src/main/java/dimhol/view/AbstractScreen.java
@@ -3,13 +3,16 @@ package dimhol.view;
 import dimhol.core.Engine;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionListener;
 
 public abstract class AbstractScreen extends JPanel{
     protected final Engine engine;
@@ -18,16 +21,32 @@ public abstract class AbstractScreen extends JPanel{
     protected JPanel centerPanel; 
     protected JLabel title;
     protected GridBagConstraints gbc;
-    public AbstractScreen(final Engine engine){
+
+    public AbstractScreen(final Engine engine) {
         this.engine = engine;
         this.centerPanel = new JPanel();
-        this.font = new Font("Helvetica", Font.BOLD,20);
+        this.font = new Font("Helvetica", Font.BOLD, 20);
         this.gbc = new GridBagConstraints();
         setBorder(new EmptyBorder(10, 10, 10, 10));
         this.setLayout(new GridBagLayout());
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.anchor = GridBagConstraints.NORTH;
     }
-    protected void paintComponent(Graphics g){
+
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(background.getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
+    }
+
+    public JButton createButton(ActionListener actionListener, String title, Color color) {
+        var button = new JButton(title);
+        button.addActionListener(actionListener);
+        button.setFont(this.font);
+        button.setForeground(color);
+        return button;
+    }
+
+    public JLabel createLabel (ImageIcon icon){
+        return new JLabel(icon);
     }
 }
--- src/main/java/dimhol/view/HomeScreen.java
+++ src/main/java/dimhol/view/HomeScreen.java
@@ -4,7 +4,6 @@ import dimhol.core.Engine;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.util.List;
 import javax.swing.*;
 
 public class HomeScreen extends AbstractScreen {
@@ -12,33 +11,15 @@ public class HomeScreen extends AbstractScreen {
     public HomeScreen(Engine engine) {
         super(engine);
         this.background = new ImageIcon("src/main/resources/asset/bg/mainMenu.png");
-        this.title = new JLabel(new ImageIcon("src/main/resources/asset/bg/dimension holiday.png"));
-        final JButton startButton = new JButton("PLAY");
-        final JButton exitButton = new JButton("QUIT");
-        final JButton optionsButton = new JButton("OPTIONS");
-        List<JButton> buttons = List.of(startButton,optionsButton,exitButton);
-        startButton.addActionListener(e -> {
-            engine.newGame();
-        });
-        exitButton.addActionListener(e -> {
-            System.exit(0);
-        });
-        optionsButton.addActionListener(e ->{
-            engine.getMainWindow().changePanel(new OptionScreen(engine));
-        });
-        for(var button: buttons){
-            button.setFont(this.font);
-            button.setForeground(new Color(102,0,153));
-        }
-        gbc.gridwidth = GridBagConstraints.REMAINDER;
-        gbc.anchor = GridBagConstraints.NORTH;
-        this.add(this.title,gbc);
+        Color color = new Color(102,0,153);
+        this.add(createLabel(new ImageIcon("src/main/resources/asset/bg/dimension holiday.png")),gbc);
         gbc.anchor = GridBagConstraints.CENTER;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         centerPanel.setLayout(new GridBagLayout());
-        centerPanel.add(startButton, gbc);
-        centerPanel.add(optionsButton, gbc);
-        centerPanel.add(exitButton, gbc);
+        centerPanel.add(createButton((e -> engine.newGame()), "PLAY", color), gbc);
+        centerPanel.add(createButton((e -> System.exit(0)), "OPTIONS", color), gbc);
+        centerPanel.add(createButton((e -> engine.getMainWindow().changePanel(new OptionScreen(engine))), "QUIT",
+            color), gbc);
         gbc.weighty = 1;
         this.add(centerPanel);
     }
