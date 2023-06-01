39786b281cddd1868f83d43c665bc6c519a10913
Emanuele Dajko <ema.unibo@gmail.com>
2023-06-01 23:51:27 +0200

    Introduces the setter, for the tile and a method to check if the tile is within a valid coordinate.

--- src/main/java/dimhol/gamelevels/map/TileMap.java
+++ src/main/java/dimhol/gamelevels/map/TileMap.java
@@ -3,7 +3,7 @@ package dimhol.gamelevels.map;
 import java.util.List;
 
 /**
- * The TileMap interface represents a map made up of tiles in a rogue-like game.
+ * The TileMap interface represents a map made up of tiles.
  */
 public interface TileMap {
 
@@ -15,6 +15,15 @@ public interface TileMap {
      * @return the tile at the specified coordinates
      */
     Tile getTile(int x, int y);
+
+    /**
+     * Sets the tile at the specified coordinates.
+     *
+     * @param x    the x-coordinate of the tile
+     * @param y    the y-coordinate of the tile
+     * @param tile the tile to be set at the specified coordinates
+     */
+    void setTile(int x, int y, Tile tile);
 
     /**
      * Returns the width of the tile map.
@@ -33,14 +42,14 @@ public interface TileMap {
     /**
      * Return the width of a single tile in the tile map.
      *
-     * @return the width of a single tile in the tile map
+     * @return the width of a single tile in the tile map.
      */
     int getTileWidth();
 
     /**
      * Return the height of a single tile in the tile map.
      *
-     * @return the height of a single tile in the tile map
+     * @return the height of a single tile in the tile map.
      */
     int getTileHeight();
 
@@ -50,4 +59,13 @@ public interface TileMap {
      * @return The layers of the tile map.
      */
     List<Tile[][]> getLayers();
+
+    /**
+     * Checks if the given coordinates are valid within the map.
+     *
+     * @param x the x-coordinate to check
+     * @param y the y-coordinate to check
+     * @return true if the coordinates are valid, false otherwise
+     */
+    boolean isValidCoordinate(int x, int y);
 }
