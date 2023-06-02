56e88345d219e05432983acb1cfdfdb59977c7a0
Alessandra Versari <alessandra.versari@studio.unibo.it>
2023-06-02 20:43:58 +0200

    Fix loading resources.

--- src/main/java/dimhol/view/PauseScreen.java
+++ src/main/java/dimhol/view/PauseScreen.java
@@ -1,19 +1,27 @@
 package dimhol.view;
 
 import dimhol.core.Engine;
-import java.util.List;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class PauseScreen extends AbstractScreen {
 
     public PauseScreen(Engine engine, final Scene scene) {
         super(engine);
-        this.background = new ImageIcon("src/main/resources/asset/bg/pauseScreen.jpg");
+        try{
+            this.background = new ImageIcon(ImageIO.read(this.getClass().getResourceAsStream("/asset/bg/pauseScreen.jpg")));
+        } catch(Exception e){
+            System.out.print("Error loading menu images");
+        }
         Color color = new Color(102,0,153);
-        this.add(createLabel(new ImageIcon("src/main/resources/asset/bg/pauseTitle.png")),gbc);
+        try{
+            this.add(createLabel((new ImageIcon(ImageIO.read(this.getClass().getResourceAsStream("/asset/bg/pauseTitle.png"))))), gbc);
+        } catch(Exception e){
+            System.out.print("Error loading menu images");
+        }
         gbc.anchor = GridBagConstraints.CENTER;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         centerPanel.setLayout(new GridBagLayout());
