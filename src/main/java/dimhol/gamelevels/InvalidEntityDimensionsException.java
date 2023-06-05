e2e901c99a0355ccfba1c983e29f823aaec72c21
Emanuele Dajko <ema.unibo@gmail.com>
2023-06-05 17:29:21 +0200

    Custom exception thrown to indicate invalid entity dimensions in a game level.

---
+++ src/main/java/dimhol/gamelevels/InvalidEntityDimensionsException.java
@@ -0,0 +1,15 @@
+package dimhol.gamelevels;
+
+/**
+ * Custom exception thrown to indicate invalid entity dimensions in a game level.
+ */
+public class InvalidEntityDimensionsException extends RuntimeException {
+    /**
+     * Constructs a new InvalidEntityDimensionsException with the specified detail message.
+     *
+     * @param message the detail message
+     */
+    public InvalidEntityDimensionsException(String message) {
+        super(message);
+    }
+}
