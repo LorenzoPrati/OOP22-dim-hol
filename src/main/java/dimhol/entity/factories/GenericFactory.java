0346220bab86a02a99b83491734f92d3d67c6c6e
Emanuele Dajko <ema.unibo@gmail.com>
2023-06-06 21:58:59 +0200

    Add the movement component and the respective speed value.

--- src/main/java/dimhol/entity/factories/GenericFactory.java
+++ src/main/java/dimhol/entity/factories/GenericFactory.java
@@ -12,6 +12,7 @@ import org.locationtech.jts.math.Vector2D;
 public class GenericFactory extends AbstractFactory {
 
     private static final double PLAYER_SPEED = 3;
+    private static final double SHOP_KEEPER_SPEED = 1;
     private static final int PLAYER_HEALTH = 10;
     private static final double PLAYER_WIDTH = 1;
     private static final double PLAYER_HEIGHT = 1;
@@ -35,9 +36,10 @@ public class GenericFactory extends AbstractFactory {
     public Entity createShopkeeper(final double x, final double y) {
         return new EntityBuilder().add(new ShoopKeeperComponent())
                 .add(new PositionComponent(new Vector2D(x,y), 0))
+                .add(new MovementComponent(new Vector2D(0, 1), SHOP_KEEPER_SPEED, false))
                 .add(new BodyComponent(new RectBodyShape(PLAYER_WIDTH, PLAYER_HEIGHT), true))
                 .add(new InteractorComponent())
-                .add(new AnimationComponent(map.get("player"),"idle down"))
+                .add(new AnimationComponent(map.get("shopkeeper"),"idle down"))
                 .build();
     }
 }
