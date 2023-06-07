ec81c955d61aa9ae2980159247b85bcf4920908c
Emanuele Dajko <ema.unibo@gmail.com>
2023-06-07 16:50:31 +0200

    Removes what is unnecessary.

--- src/main/java/dimhol/components/ChargeAttackComponent.java
+++
@@ -1,9 +0,0 @@
-package dimhol.components;
-
-import dimhol.components.Component;
-
-/**
- * Represents a component that enables an entity to perform a charge attack.
- */
-public class ChargeAttackComponent implements Component {
-}
